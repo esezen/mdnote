12e99564984abad4be863a9243508928532a9d92
Enes Kutay SEZEN <eneskutaysezen@Eness-MacBook-Pro.local>
2018-08-04 18:25:53 +0300

    Firebase, axios and element ui

--- src/store.js
+++ src/store.js
@@ -1,16 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// import firebase from '@/firebaseConfig'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state: {},
+  mutations: {},
+  actions: {}
 })
+
+// export default new Vuex.Store({
+//   state: {
+//     orders: []
+//   },
+//   mutations: {
+//     fetchOrdersSuccess(state, payload) {
+//       var orders = []
+//       for (const [key, value] of Object.entries(payload)) {
+//         orders.push(value)
+//       }
+//       state.orders = orders
+//     }
+//   },
+//   actions: {
+//     fetchOrders({ commit, state }) {
+//       const ordersRef = firebase.database().ref('orders')
+//       ordersRef.once('value', snapshot => {
+//         let orders = snapshot.val()
+//         commit('fetchOrdersSuccess', orders)
+//       })
+//     }
+//   }
+// })
