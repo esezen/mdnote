925810cb954cdd79fd28ffcdf38af0d374cb94f7
Enes Kutay SEZEN <eneskutaysezen@Eness-MacBook-Pro.local>
2018-08-11 17:20:31 +0300

    Just realized there is an easier way

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-aside>
-        <navbar/>
-
-      </el-aside>
-      <el-main>
-
-        <router-view/>
-      </el-main>
-    </el-container>
+    <router-view></router-view>
   </div>
 </template>
-<script>
-import NavBar from '@/components/NavBar'
-export default {
-  components: {
-    navbar: NavBar
-  }
-}
-</script>
 
-<style>
-@import url('https://fonts.googleapis.com/css?family=Quicksand');
-html,
-body,
-div {
-  font-family: 'Quicksand', sans-serif !important;
-  margin: 0;
-  padding: 0;
-  border: 0;
-}
-</style>
+<script>
+  export default{
+    name: 'App'
+  }
+</script>
